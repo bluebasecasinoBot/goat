985ab3456d68e3b2cdc6907510ab7fac64278b95
bluebasecasinoBot <developerme33@gmail.com>
2024-07-16 14:16:21 -0700

    Update __index.js

--- static/__index.js
+++ static/__index.js
@@ -180,6 +180,7 @@ __SYD.nav_b_text = (text,click) =>{
                         break;
                         default:
                             __v[click].scrollIntoView();
+                            alert('done')
                             // __v['mobileMenu'].blur()
                             
                     }
